cc9f0c48f99eb70721e6eaa1ba9f3471859b89b3
IslamAhmedDiv <IslamDev.sj@gmail.com>
2024-03-28 22:38:48 +0200

    handling error: audio not found

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
+function catchError(fn, message){
+    try{
+        fn()
+    }catch(error){
+        console.log(message)
+    }
+}
+
 function audioRange(){
-    const audioRange = document.getElementById("audioRange")
+    const audioFile = document.getElementById("audioFile")
+    if(!audioFile.duration){ return}
     const durAud = document.getElementById("durAud")
     const curTime = document.getElementById("curTime")
-    const inputRange = document.getElementById("inputRangeAudio")
-    const dur = Math.floor(audioRange.duration)
+    const rangeAudio = document.getElementById("customRangeAudio")
+    const dur = Math.floor(audioFile.duration)
     let min = Math.floor(dur/60)
     let sec = dur % 60
-    inputRange.max = dur
-    function addZero(num) {
-        if (num < 10){ num ="0"+ num; return num }else {return num}
-    }
-    inputRange.oninput = ()=>{
-        let durRng = inputRange.value
+    rangeAudio.max = dur
+    function addZero(num) {if (num < 10){ num ="0"+ num; return num }else {return num}}
+
+    rangeAudio.oninput = ()=>{
+        let durRng = rangeAudio.value
         let min = Math.floor(durRng/60)
         let sec = durRng % 60
-        audioRange.currentTime = durRng
+        audioFile.currentTime = durRng
+
         curTime.innerHTML= `${addZero(min)}:${addZero(sec)}/`
-        audioRange.play()
+        audioFile.play()
 
     }
     curTime.innerHTML= `00:00 /`
@@ -25,14 +34,14 @@ function audioRange(){
 
     let iconPlay =document.getElementById("iconPlay")
     iconPlay.onclick = ()=>{
-        console.log(audioRange.paused )
-        if (audioRange.paused === true) {
-            audioRange.play()
+        console.log(audioFile.paused )
+        if (audioFile.paused === true) {
+            audioFile.play()
             iconPlay.classList.add("fa-pause")
             iconPlay.classList.remove("fa-play")
         }
         else {
-            audioRange.pause()
+            audioFile.pause()
             iconPlay.classList.add("fa-play")
             iconPlay.classList.remove("fa-pause")
 
